299ea9bf1c31f9a78711515ec0be8318316d6432
Aaron Gaut <git@aarongaut.com>
2022-04-01 15:34:03 -0700

    Removed RandomContext

--- tests/common/testutils.py
+++ tests/common/testutils.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import random
 
 
 def firstn(iterable, n):
@@ -13,25 +12,8 @@ def firstn(iterable, n):
     return values
 
 
-def pwdrel(paths):
-    cwd = Path(".").resolve()
-    return [str(Path(p).relative_to(cwd)) for p in paths]
+def rootrel(paths):
+    from nextsong.config import get as get_cfg
 
-
-class RandomContext:
-    def __init__(self, seed=None):
-        self.idle_state = random.getstate()
-        random.seed(seed)
-        self._swap()
-
-    def _swap(self):
-        tmp = random.getstate()
-        random.setstate(self.idle_state)
-        self.idle_state = tmp
-
-    def __enter__(self):
-        self._swap()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._swap()
+    root = get_cfg("media_root")
+    return [str(Path(p).relative_to(root)) for p in paths]
