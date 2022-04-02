9eada071994f51b100b224eb0a499bead0df3b39
Aaron Gaut <git@aarongaut.com>
2022-04-02 07:54:17 -0700

    Updated test for addition of newline in nextsong output

--- tests/cases/examples/basic/test.sh
+++ tests/cases/examples/basic/test.sh
@@ -2,16 +2,13 @@ set -e
 
 python gen_playlist.py
 
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
-
-printf "$(nextsong)\n"
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
+nextsong
