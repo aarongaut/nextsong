f22d2d91d768cfa5384df259f682515116c7d617
Aaron Gaut <git@aarongaut.com>
2022-04-03 05:35:42 -0700

    Added tee append arg where suitable

--- tests/cases/cli/test.sh
+++ tests/cases/cli/test.sh
@@ -7,8 +7,8 @@ export NEXTSONG_PLAYLIST_PATH="$RL_ROOT/tests/common/sample_playlists/1.xml"
 export NEXTSONG_STATE_PATH="./artifacts/state.pickle"
 
 printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee artifacts/output.txt
-printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee artifacts/output.txt
-printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee artifacts/output.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/output.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/output.txt
 
 printf "Diffing output - <got >exp\n"
 diff -r artifacts/output.txt expected/output.txt
