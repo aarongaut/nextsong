c27e7557d30e9df4871fb45afb20050388a7d51a
Aaron Gaut <git@aarongaut.com>
2022-04-28 07:04:56 -0700

    Scrubbed machine specific part of paths

--- tests/cases/playlist/on_change/restart/test.sh
+++ tests/cases/playlist/on_change/restart/test.sh
@@ -8,19 +8,18 @@ export NEXTSONG_STATE_PATH="./artifacts/state.pickle"
 export NEXTSONG_ON_CHANGE=restart
 
 cp "$RL_ROOT/tests/common/sample_playlists/1.xml" "$NEXTSONG_PLAYLIST_PATH"
-
-nextsong | tee -a artifacts/out.txt
-nextsong | tee -a artifacts/out.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/out.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/out.txt
 
 printf "Touching playlist; should restart\n" | tee -a artifacts/out.txt
 touch "$NEXTSONG_PLAYLIST_PATH"
 
-nextsong | tee -a artifacts/out.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/out.txt
 
 printf "Touching playlist; should restart again\n" | tee -a artifacts/out.txt
 touch "$NEXTSONG_PLAYLIST_PATH"
 
-nextsong | tee -a artifacts/out.txt
+printf "$(realpath --relative-to=$NEXTSONG_MEDIA_ROOT $(nextsong))\n" | tee -a artifacts/out.txt
 
 printf "Diffing output - <got >exp\n"
 diff artifacts/out.txt expected/out.txt
