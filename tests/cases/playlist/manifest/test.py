68dcdff0acee4d94c422f292beaebc2c9422a949
Aaron Gaut <git@aarongaut.com>
2022-04-28 05:27:03 -0700

    Format with black

--- tests/cases/playlist/manifest/test.py
+++ tests/cases/playlist/manifest/test.py
@@ -1,9 +1,11 @@
 from nextsong import Playlist as p
 from testutils import rootrel
 
+
 def m(playlist):
     """Get a playlist's manifest in a consistent form for testing"""
     return rootrel(sorted(set(playlist.manifest())))
+
 
 assert m(p()) == []
 assert m(p("1.mp3", "2.mp3", "3.mp3")) == ["1.mp3", "2.mp3", "3.mp3"]
